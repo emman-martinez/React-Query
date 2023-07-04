45b3316db18dfb17838d36862f6dc2c77a848fc0
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2023-07-04 12:23:48 -0600

    Your First Query

---
+++ vite-your-first-query/src/App.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import { useQuery } from "@tanstack/react-query";
+import "./App.css";
+
+function fetchUser(username) {
+  return fetch(`https://api.github.com/users/${username}`).then((res) =>
+    res.json()
+  );
+}
+
+function GithubUser({ username }) {
+  const userQuery = useQuery([username], () => fetchUser(username));
+
+  const { data, error, isError, isLoading } = userQuery;
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (isError) return <p>{error.message}</p>;
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
+
+GithubUser.propTypes = {
+  username: PropTypes.string.isRequired,
+};
+
+function App() {
+  return (
+    <>
+      <GithubUser username="uidotdev" />
+    </>
+  );
+}
+
+export default App;
