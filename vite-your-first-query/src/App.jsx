9be4d911bc7d1dbe08e6e5cde8f9d9764d519812
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2023-07-20 14:30:52 -0600

    ERROR HANDLING

--- vite-your-first-query/src/App.jsx
+++ vite-your-first-query/src/App.jsx
@@ -2,6 +2,22 @@ import PropTypes from "prop-types";
 import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
+async function fetchWithError(url, options) {
+  const response = await fetch(url, options);
+
+  if (response.status !== 200) {
+    throw new Error("Error in request!");
+  }
+
+  const result = await response.json();
+
+  if (result.error) {
+    throw new Error(result.error);
+  }
+
+  return result;
+}
+
 function fetchUser(username) {
   return fetch(`https://api.github.com/users/${username}`).then((res) =>
     res.json()
